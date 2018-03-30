1c37e28f1b1baf59f4bda18700c96ebe154ea4f4
cyaah <calvin.yah@gmail.com>
2018-03-30 12:36:43 -0400

    Addedfeature for admin credentials to access certain pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
 	end
 
 	def user_params
-		params.require(:user).permit(:name, :password, :password_confirmation)
+		params.require(:user).permit(:firstname, :lastname, :email, :password, :password_confirmation)
 	end
 
 end
